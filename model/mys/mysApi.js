0b495dd5808435bf4aeaf8661dc1e9741c7f0452
kissnavel <xkn0722@gmail.com>
2024-10-16 18:23:54 +0800

    fix:遇到验证码时无法获取角色信息

--- model/mys/mysApi.js
+++ model/mys/mysApi.js
@@ -142,8 +142,6 @@ export default class MysApi {
 
     res.api = type
 
-    if(res.api === 'character' && res.data?.list) res.data.avatars = res.data?.list
-
     if (cached) this.cache(res, cacheKey)
 
     return res
