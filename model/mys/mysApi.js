bd1cf594867af2f51c6844e66441cb4bce39325a
kissnavel <xkn0722@gmail.com>
2025-06-14 16:08:30 +0800

    update:默认设备

--- model/mys/mysApi.js
+++ model/mys/mysApi.js
@@ -70,14 +70,14 @@ export default class MysApi {
         headers['x-rpc-device_model'] = modelName
         headers['x-rpc-csm_source'] = 'myself'
         // 国际服不需要绑定设备，故写入的'User-Agent'为国服
-        headers['User-Agent'] = `Mozilla/5.0 (Linux; Android ${osVersion}; ${modelName} Build/${deviceDisplay}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.4896.88 Mobile Safari/537.36 miHoYoBBS/2.73.1`
+        headers['User-Agent'] = `Mozilla/5.0 (Linux; Android ${osVersion}; ${modelName} Build/${deviceDisplay}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 miHoYoBBS/2.73.1`
       } catch (error) {
         logger.error(`[genshin]设备信息解析失败：${error.message}`)
       }
     } else {
       try {
-        headers['x-rpc-device_name'] = 'Sony XQ-AT52'
-        headers['x-rpc-device_model'] = 'XQ-AT52'
+        headers['x-rpc-device_name'] = 'Sony XQ-BC52'
+        headers['x-rpc-device_model'] = 'XQ-BC52'
         headers['x-rpc-csm_source'] = 'myself'
       } catch (error) {
         logger.error(`[genshin]设备信息解析失败：${error.message}`)
@@ -253,7 +253,7 @@ export default class MysApi {
   getHeaders(query = '', body = '') {
     const cn = {
       app_version: '2.73.1',
-      User_Agent: 'Mozilla/5.0 (Linux; Android 12; XQ-AT52 Build/58.2.A.7.93; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.4896.88 Mobile Safari/537.36 miHoYoBBS/2.73.1',
+      User_Agent: 'Mozilla/5.0 (Linux; Android 13; XQ-BC52 Build/61.2.A.0.472A; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 miHoYoBBS/2.73.1',
       client_type: '5',
       Origin: 'https://webstatic.mihoyo.com',
       X_Requested_With: 'com.mihoyo.hyperion',
@@ -261,7 +261,7 @@ export default class MysApi {
     }
     const os = {
       app_version: '2.57.1',
-      User_Agent: 'Mozilla/5.0 (Linux; Android 12; XQ-AT52 Build/58.2.A.7.93; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.4896.88 Mobile Safari/537.36 miHoYoBBSOversea/2.57.1',
+      User_Agent: 'Mozilla/5.0 (Linux; Android 13; XQ-BC52 Build/61.2.A.0.472A; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 miHoYoBBSOversea/2.57.1',
       client_type: '2',
       Origin: 'https://act.hoyolab.com',
       X_Requested_With: 'com.mihoyo.hoyolab',
