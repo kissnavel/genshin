3384bf41aebdb5cef5e705d114fc890257df37c5
kissnavel <xkn0722@gmail.com>
2025-09-22 21:40:16 +0800

    feat:锅巴配置

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import fs from 'node:fs'
 import Cfg from './model/Cfg.js'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'node:path'
 
 if (!fs.existsSync(Cfg.file))
   fs.mkdirSync(Cfg.file)
@@ -43,6 +45,22 @@ try {
   logger.mark('[genshin]配置文件更新完成')
 } catch (error) {
   logger.error(error)
+}
+
+/** genshin-plugin目录 */
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const genshinPluginDir = join(__dirname, '..', 'genshin-plugin')
+
+if (!fs.existsSync(genshinPluginDir)) {
+  fs.mkdirSync(genshinPluginDir, { recursive: true })
+  logger.info(`genshin-plugin目录创建成功: ${genshinPluginDir}`)
+  const guobaSupportContent = "export { supportGuoba } from '../genshin/guoba.support.js'"
+  fs.writeFileSync(join(genshinPluginDir, 'guoba.support.js'), guobaSupportContent)
+  logger.info('guoba.support.js 文件已创建')
+  const indexJsContent = 'export { }'
+  fs.writeFileSync(join(genshinPluginDir, 'index.js'), indexJsContent)
+  logger.info('index.js 文件已创建')
 }
 
 const files = fs.readdirSync('./plugins/genshin/apps').filter(file => file.endsWith('.js'))
