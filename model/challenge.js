0c8d79926f66bc9ddd14014c16211ad4ec864ddf
kissnavel <xkn0722@gmail.com>
2026-06-11 21:48:52 +0800

    feat:星铁深渊星启模式

--- model/challenge.js
+++ model/challenge.js
@@ -182,6 +182,16 @@ export default class srChallenge extends base {
     }
     const data = { ...challengeData.data }
 
+    // 根据 scheduleType 选择对应的 group（末日幻影/虚构叙事的 groups 包含本期和上期）
+    if (data.groups && data.groups.length > 1) {
+      const activeGroup = scheduleType === '1'
+        ? data.groups.find(g => g.status === 'New')
+        : data.groups.find(g => g.status === 'End')
+      if (activeGroup) {
+        data.groups = [activeGroup]
+      }
+    }
+
     if (recent && challengeType == 3) return {
       data,
       uid,
@@ -220,7 +230,15 @@ export default class srChallenge extends base {
             ...(floor.node_2.challenge_time && {
               challengeTime: this.timeFormat(floor.node_2.challenge_time, 'YYYY.MM.DD HH:mm')
             })
-          }
+          },
+          ...(floor.node_3 && {
+            node_3: {
+              ...floor.node_3,
+              ...(floor.node_3.challenge_time && {
+                challengeTime: this.timeFormat(floor.node_3.challenge_time, 'YYYY.MM.DD HH:mm')
+              })
+            }
+          })
         }
       })
     } else {
@@ -245,9 +263,17 @@ export default class srChallenge extends base {
     // 末日幻影、虚构叙事：计算两边节点的总分
     if ([0, 1].includes(challengeType)) {
       data.all_floor_detail = _.map(data.all_floor_detail, (floor) => {
-        return {
-          ...floor,
-          score: (parseInt(floor.node_1.score) + parseInt(floor.node_2.score)).toString()
+        if (floor.node_1.score != null) {
+          let totalScore = parseInt(floor.node_1.score) + parseInt(floor.node_2.score)
+          if (floor.node_3 && floor.is_tierce) {
+            totalScore += parseInt(floor.node_3.score)
+          }
+          return {
+            ...floor,
+            score: totalScore.toString()
+          }
+        } else {
+          return floor
         }
       })
     }
